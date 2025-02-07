66d88aaa8345576011e8311246fa9259fce73723
valerie-wong <74160103+valerie-wong@users.noreply.github.com>
2025-02-07 11:19:17 -0500

    static path edited

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from whitenoise import WhiteNoise
 
 
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/", prefix="static/", index_file="index.html", autorefresh=True)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/", prefix="/", index_file="index.html", autorefresh=True)
 
 # You can change the prefix up above and get rid of this route if you just want to host static (which begs the question why you don't use something simpler like Github Pages)
 
 if __name__ == "__main__":
-  app.run(threaded=True, port=9000)
+  app.run(threaded=True, port=5000)
